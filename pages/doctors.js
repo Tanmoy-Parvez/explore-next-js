49229acb6311f848f802b599e8e2a8ee680d348e
Siam-Parvez <tanmoyparvez23@gmail.com>
2021-11-17 19:24:32 +0600

    create next js component

---
+++ pages/doctors.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const doctors = () => {
+    return (
+        <div>
+            <h1>This is next js doctors component</h1>
+        </div>
+    );
+};
+
+export default doctors;
